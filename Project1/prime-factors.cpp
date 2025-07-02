fec54db80b777e9a8639ac60fa5d07f6e2d68d57
hyeongseok.choi <babmukja0322@gmail.com>
2025-07-02 10:02:57 +0900

    case 3에 대한 TDD

--- Project1/prime-factors.cpp
+++ Project1/prime-factors.cpp
@@ -8,6 +8,9 @@ public:
         if (num == 2) {
             result.push_back(2);
         }
+        if (num == 3) {
+            result.push_back(3);
+        }
         return result;
     }
 };
--- Project1/prime-factors_test.cpp
+++ Project1/prime-factors_test.cpp
@@ -17,3 +17,8 @@ TEST_F(PrimeFactorsFixture, Of2)
     expected = { 2 };
     EXPECT_EQ(expected, primeFactors.of(2));
 }
+TEST_F(PrimeFactorsFixture, Of3)
+{
+    expected = { 3 };
+    EXPECT_EQ(expected, primeFactors.of(3));
+}
