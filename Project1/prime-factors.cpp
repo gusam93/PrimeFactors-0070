a877885771819279d9b9f9acaa26a3f9515efa43
hyeongseok.choi <babmukja0322@gmail.com>
2025-07-02 09:59:07 +0900

    case 2에 대한 TDD 진행

--- Project1/prime-factors.cpp
+++ Project1/prime-factors.cpp
@@ -5,6 +5,9 @@ class PrimeFactors {
 public:
     vector<int> of(int num) {
         vector<int> result = {};
+        if (num == 2) {
+            result.push_back(2);
+        }
         return result;
     }
 };
--- Project1/prime-factors_test.cpp
+++ Project1/prime-factors_test.cpp
@@ -8,3 +8,9 @@ TEST(PrimeFactors, Of1)
     vector<int> expected = {};
     EXPECT_EQ(expected, primeFactors.of(1));
 }
+TEST(PrimeFactors, Of2)
+{
+    PrimeFactors primeFactors;
+    vector<int> expected = {2};
+    EXPECT_EQ(expected, primeFactors.of(2));
+}
